06998d745f75d719545315b4c8b273e3b388668e
alsfkl222 <alskfl222@kakao.com>
2022-12-20 18:18:03 +0900

    [ FEAT ] : Summary ing...

--- src/lib/components/Summary.svelte
+++ src/lib/components/Summary.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
-  import { store, bossReport } from '../../stores';
+	import { store, bossReport } from '../../stores';
 	import { getTotalBossCount, getTotalBossPrice } from '../../utils';
+	import Hbar from './common/Hbar.svelte';
+	import Title from './common/Title.svelte';
 	$: charCount = $store.length;
-  $: bossCount = getTotalBossCount($bossReport)
-	$: bossPrice = getTotalBossPrice($store)
+	$: bossCount = getTotalBossCount($bossReport);
+	$: bossPrice = getTotalBossPrice($store);
+	$: countStatus = bossCount < 180 ? '더 가능!' : bossCount === 180 ? '꽉 채움!' : '과다';
 </script>
 
-<div class='bg-white'>
-	{charCount} 캐릭터 {bossCount} 보스 {bossPrice} 원
+<div class="flex flex-col justify-end rounded-2xl bg-white gap-8">
+	<Title text="요약" />
+	<div>
+		{charCount} 캐릭터
+	</div>
+	<div>
+		{bossCount} 보스
+	</div>
+	<Hbar />
+	<div>
+		{bossPrice} 원
+	</div>
+	<div>
+		{countStatus}
+	</div>
 </div>
