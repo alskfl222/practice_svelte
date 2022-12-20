187751bb42392b0d85ea56f6c342849add20c32f
alsfkl222 <alskfl222@kakao.com>
2022-12-20 10:26:53 +0900

    [ FEAT ] : CharViewer scroll

--- src/lib/components/CharViewer.svelte
+++ src/lib/components/CharViewer.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { onDestroy } from 'svelte';
 	import { store, charIndex } from '../../stores';
 
 	let charContainer: HTMLDivElement;
+	let charScroll: HTMLElement;
 
 	function selectChar(e: MouseEvent, idx: number) {
 		const el = e.target as HTMLDivElement;
@@ -14,10 +16,59 @@
 	function deleteChar(idx: number) {
 		$store = [...$store.slice(0, idx), ...$store.slice(idx + 1)];
 		$charIndex = undefined;
+		charScroll.style.left = '0px';
 	}
+
+	$: dragChar = (node: HTMLElement) => {
+		let moving = false;
+		let x = 0;
+		const minX = 0;
+		const maxX = $store.length * (270 + 16) - 904;
+
+		node.style.position = 'relative';
+		node.style.left = `${x}px`;
+		node.style.userSelect = 'none';
+
+		node.addEventListener('mousedown', () => {
+			moving = true;
+		});
+		window.addEventListener('mousemove', (e) => {
+			if (!moving) return;
+			e.preventDefault();
+			x += e.movementX;
+			if (minX < maxX) {
+				if (x > minX) x = 0;
+				if (x < -maxX) x = -maxX;
+				node.style.left = `${x}px`;
+			}
+		});
+		window.addEventListener('mouseup', () => {
+			moving = false;
+		});
+
+		onDestroy(() => {
+			window.addEventListener('mousemove', (e) => {
+				if (moving) {
+					x += e.movementX;
+					if (minX < maxX) {
+						if (x > minX) x = 0;
+						if (x < -maxX) x = -maxX;
+						node.style.left = `${x}px`;
+					}
+				}
+			});
+			window.addEventListener('mouseup', () => {
+				moving = false;
+			});
+		});
+	};
 </script>
 
-<div class="flex flex-col" bind:this={charContainer} on:click={(e) => selectChar(e, -1)}>
+<div
+	class="w-[904px] flex flex-col overflow-hidden"
+	bind:this={charContainer}
+	on:click={(e) => selectChar(e, -1)}
+>
 	<div class="p-4 font-bold">
 		{#if $charIndex === undefined}
 			<div>
@@ -26,17 +77,17 @@
 		{:else}
 			<div>{$store[$charIndex].name} {$store[$charIndex].class}</div>{/if}
 	</div>
-	<div class="p-2 flex gap-4">
+	<div class="p-2 flex gap-4" bind:this={charScroll} use:dragChar>
 		{#each $store as char, idx}
 			<div
 				class={idx === $charIndex
-					? 'w-[200px] bg-gradient-to-r from-cyan-500 to-blue-500 rounded'
-					: 'w-[200px] border rounded border-cyan-500'}
+					? 'w-[270px] flex-none bg-gradient-to-r from-cyan-500 to-blue-500 rounded'
+					: 'w-[270px] flex-none border rounded border-cyan-500'}
 			>
 				<div class="p-4 flex justify-between" on:click|stopPropagation={(e) => selectChar(e, idx)}>
 					<div class="flex gap-2">
-						<span>{char.name || '이름 없음'}</span>
-						<span>{char.class || '직업 없음'}</span>
+						<span>{char.name}</span>
+						<span>{char.class}</span>
 					</div>
 					<button on:click|stopPropagation={() => deleteChar(idx)}>삭제</button>
 				</div>
--- src/lib/components/Summary.svelte
+++ src/lib/components/Summary.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { store, bossReport } from '../../stores';
-	import { getTotalBossCount } from '../../utils';
+	import { getTotalBossCount, getTotalBossPrice } from '../../utils';
 	$: charCount = $store.length;
   $: bossCount = getTotalBossCount($bossReport)
+	$: bossPrice = getTotalBossPrice($store)
 </script>
 
 <div>
-	{charCount} 캐릭터 {bossCount} 보스
+	{charCount} 캐릭터 {bossCount} 보스 {bossPrice} 원
 </div>
