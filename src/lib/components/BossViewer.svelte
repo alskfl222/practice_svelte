7b3b1efd031e9942adbf3ef880369e1ce83b28d2
alsfkl222 <alskfl222@kakao.com>
2022-12-22 14:01:42 +0900

    [ FEAT ] : add BossViewer boss option

--- src/lib/components/BossViewer.svelte
+++ src/lib/components/BossViewer.svelte
@@ -2,6 +2,8 @@
 	import type { BossDC } from 'src/types';
 	import { store, charIndex } from '../../stores';
 
+	let count: number = 0;
+
 	function deleteBoss(idx: number, dc: keyof BossDC) {
 		if ($charIndex !== undefined) {
 			let charBossDC = $store[$charIndex].boss[idx].dc;
@@ -25,7 +27,6 @@
 		}
 		return count;
 	}
-	let count = 0;
 
 	$: if ($charIndex !== undefined) {
 		count = getCharBossCount($store[$charIndex].boss);
@@ -44,23 +45,34 @@
 		{#if $store[$charIndex] && $store[$charIndex].boss.length > 0}
 			<div class="p-4 flex flex-col gap-4">
 				{#each $store[$charIndex].boss as boss, idx}
-					<div class="w-full h-[80px]">
+					<div class="w-full h-[90px]">
 						<div class="h-[100%] flex justify-between items-center">
 							<div
-								class="w-[480px] px-12 py-4 rounded-3xl bg-cover bg-center bg-no-repeat text-4xl text-white font-bold"
+								class="w-[480px] h-[100%] px-12 py-4 flex items-center rounded-3xl
+												bg-cover bg-center bg-no-repeat text-4xl text-white font-bold"
 								style={`background-image: url(${boss.image})`}
 							>
 								{boss.name}
 							</div>
-							<div class="flex">
+							<div class="h-[100%]">
 								{#each boss.dc as dc}
-									<span
+									<div
 										on:click={() => deleteBoss(idx, dc[0])}
-										class="p-1 rounded drop-shadow-lg cursor-pointer
+										class="h-[100%] px-4 flex flex-col justify-center gap-2
+													 border rounded-2xl drop-shadow-lg cursor-pointer
 													 hover:bg-gray-500/30 transition duration-100 ease-in-out"
 									>
-										{dc}
-									</span>
+										<span>{dc[0]}</span>
+										<div class="w-full flex justify-center gap-2">
+											<span>{dc[1]}인</span><span
+												>{#if dc[2]}
+													<i class="fa-solid fa-exclamation" />
+												{:else}
+													<i class="fa-solid fa-question" />
+												{/if}</span
+											>
+										</div>
+									</div>
 								{/each}
 							</div>
 						</div>
@@ -70,5 +82,5 @@
 		{/if}
 	</div>
 {:else}
-		<div class="mt-4 px-8 py-4 text-xl font-bold text-slate-700">선택된 캐릭터가 없습니다</div>
+	<div class="mt-4 px-8 py-4 text-xl font-bold text-slate-700">선택된 캐릭터가 없습니다</div>
 {/if}
