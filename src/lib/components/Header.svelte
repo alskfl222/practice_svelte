371301dfa286060b089c2d166e86b1e329619ac7
alsfkl222 <alskfl222@kakao.com>
2023-01-11 18:14:13 +0900

    [ FIX ] : Header icon position

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,17 +6,17 @@
 </script>
 
 <header class="w-full h-[90px] mb-8 px-12 rounded-b-3xl bg-white">
-	<nav class="w-full h-full">
-		<ul class="w-full h-full flex justify-between items-center">
+	<nav class="w-full h-full flex justify-between items-center">
+		<ul class="w-full h-full flex items-center gap-9">
 			<li class={$page.url.pathname === '/' ? selectedStyle : normalStyle}>
 				<a href="/"><i class="fa-solid fa-house" /></a>
 			</li>
 			<li class={$page.url.pathname === '/calendar' ? selectedStyle : normalStyle}>
 				<a href="/calendar"><i class="fa-solid fa-calendar"></i></a>
 			</li>
-			<li class={$page.url.pathname === '/about' ? selectedStyle : normalStyle}>
-				<a href="/about"><i class="fa-solid fa-circle-question" /></a>
-			</li>
 		</ul>
+			<div class={$page.url.pathname === '/about' ? selectedStyle : normalStyle}>
+				<a href="/about"><i class="fa-solid fa-circle-question" /></a>
+			</div>
 	</nav>
 </header>
