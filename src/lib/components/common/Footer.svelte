fa81a2f7e3dbf323196451f6bb505667bd316afa
alsfkl222 <alskfl222@kakao.com>
2023-01-17 15:06:32 +0900

    [ FEAT ] : init dark mode, add toggle

--- src/lib/components/common/Footer.svelte
+++ src/lib/components/common/Footer.svelte
@@ -1,13 +1,13 @@
-<footer class="w-full h-[100px] pt-8 flex justify-center gap-12 bg-white text-xl">
+<footer class="w-full h-[100px] pt-8 flex justify-center gap-12 bg-white dark:bg-slate-900 text-xl">
 	<div class='w-full max-w-[1200px] px-4 sm:px-12 flex justify-between'>
-		<span>by <strong>alskfl222</strong></span>
+		<span class='dark:text-slate-100'>by <strong>alskfl222</strong></span>
 		<div class="flex gap-8">
-			<span class="text-3xl"
+			<span class="text-3xl dark:text-slate-100"
 				><a href="https://github.com/alskfl222/practice_svelte/"
 					><i class="fa-brands fa-github" /></a
 				></span
 			>
-			<span class="text-3xl"
+			<span class="text-3xl dark:text-slate-100"
 				><a href="mailto: alskfl222@kakao.com"><i class="fa-solid fa-envelope" /></a></span
 			>
 		</div>
--- src/lib/components/common/Header.svelte
+++ src/lib/components/common/Header.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { darkMode } from '$stores/mode';
+	import Toggle from './Toggle.svelte';
 
-	const selectedStyle = 'text-3xl font-bold';
-	const normalStyle = 'text-lg'
+	const selectedStyle = 'text-3xl font-bold dark:text-slate-200';
+	const normalStyle = 'text-lg dark:text-slate-200';
+
+	$: if ($darkMode) {
+		localStorage.setItem('darkMode', 'true');
+	} else {
+		localStorage.setItem('darkMode', 'false');
+	}
 </script>
 
-<header class="w-full h-[90px] mb-8 px-12 rounded-b-3xl bg-white">
+<header class="w-full h-[90px] mb-8 px-12 rounded-b-3xl bg-white dark:bg-slate-600">
 	<nav class="w-full h-full flex justify-between items-center">
 		<ul class="w-full h-full flex items-center gap-9">
 			<li class={$page.url.pathname === '/' ? selectedStyle : normalStyle}>
 				<a href="/"><i class="fa-solid fa-house" /></a>
 			</li>
 			<li class={$page.url.pathname === '/calendar' ? selectedStyle : normalStyle}>
-				<a href="/calendar"><i class="fa-solid fa-calendar"></i></a>
+				<a href="/calendar"><i class="fa-solid fa-calendar" /></a>
 			</li>
 		</ul>
+		<div class="flex flex-col xs:flex-row items-center gap-2">
 			<div class={$page.url.pathname === '/about' ? selectedStyle : normalStyle}>
 				<a href="/about"><i class="fa-solid fa-circle-question" /></a>
 			</div>
+			<div class="flex items-center gap-2">
+				<Toggle bind:value={$darkMode} />
+				<span class:text-white={$darkMode}><i class="fa-solid fa-lightbulb" /></span>
+			</div>
+		</div>
 	</nav>
 </header>
